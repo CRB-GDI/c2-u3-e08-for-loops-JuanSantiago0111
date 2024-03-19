16b2107db5f2bc90653fda47a8eef1c4f82ef623
JuanSantiago0111 <145376152+JuanSantiago0111@users.noreply.github.com>
2024-03-19 13:16:57 -0400

    ready to grade

---
+++ exercise8/exercise8/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel;
+
+namespace exercise8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Enter number of evens (2-10):");
+            int n = int.Parse(Console.ReadLine());
+            
+            // print the evens from 2 to 10
+            for (int i = 0; i <= n; i+=2)
+            {
+                Console.WriteLine(i);
+            }
+            // problem 2 
+            Console.WriteLine("enter number of asterisk");
+           int asterisks = int.Parse(Console.ReadLine());
+            // print asterisk 5 times in a row 
+              
+            for (int i = 0; i < asterisks; i++)
+            {
+                Console.Write("*");
+            }
+        }
+    }
+}
